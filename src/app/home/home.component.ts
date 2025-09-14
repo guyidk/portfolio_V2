371fa56c77e0844e5aa13a7adf11bc2a0b8e3dd8
guyidk <126870886+guyidk@users.noreply.github.com>
2025-09-14 09:56:34 +0800

    Testing

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+  dynamicText: string = "";
+  phrases: string[] = [
+    "Team Player.",
+    "Software Developer.",
+    "Backend Engineer.",
+    "Software Engineer.",
+    "Fullstack Developer.",
+    "Code Enthusiast.",
+  ];
+
+  currentPhraseIndex: number = 0;
+  currentCharIndex: number = 0;
+  isDeleting: boolean = false;
+  typingSpeed: number = 100;
+  deletingSpeed: number = 100;
+  delayBetweenPhrases: number = 2000;
+
+    ngOnInit(): void {
+    this.type();
+  }
+
+  type() {
+    const currentPhrase = this.phrases[this.currentPhraseIndex];
+    if (this.isDeleting) {
+      this.dynamicText = currentPhrase.substring(0, this.currentCharIndex - 1);
+      this.currentCharIndex--;
+    } else {
+      this.dynamicText = currentPhrase.substring(0, this.currentCharIndex + 1);
+      this.currentCharIndex++;
+    }
+
+    if (!this.isDeleting && this.currentCharIndex === currentPhrase.length) {
+      this.isDeleting = true;
+      setTimeout(() => this.type(), this.delayBetweenPhrases);
+    } else if (this.isDeleting && this.currentCharIndex === 0) {
+      this.isDeleting = false;
+      this.currentPhraseIndex = (this.currentPhraseIndex + 1) % this.phrases.length;
+      setTimeout(() => this.type(), 500);
+    } else {
+      const speed = this.isDeleting ? this.deletingSpeed : this.typingSpeed;
+      setTimeout(() => this.type(), speed);
+    }
+  }
+
+}
